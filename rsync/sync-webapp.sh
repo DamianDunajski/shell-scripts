6ee662f48aa10770347928f8d49ccf1bfe339b25
Damian Dunajski <damdun@gmail.com>
2012-09-04 23:46:57 +0200

    Added Java web application resources synchronization scripts

---
+++ rsync/sync-webapp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# != 1 ]
+then
+    echo "Usage: $0 destination";
+    echo "       - destination: a location where files should be copied"
+    exit;
+fi
+
+rsync_command="rsync -rlpt --delete --exclude=/META-INF --exclude=/WEB-INF/lib --exclude=/WEB-INF/classes --exclude=*.xml"
+rsync_src="src/main/webapp/"
+rsync_dst=${1}
+
+if [ ! -e $rsync_src ]
+then
+    echo "Source directory [$rsync_src] doesn't exist";
+    exit;
+fi
+
+eval $rsync_command ${rsync_src} ${rsync_dst}
